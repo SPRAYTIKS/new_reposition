c40f54d28efdb4810417f9ddb4ce5bd9e548650f
SPRAYTIKS <159346143+SPRAYTIKS@users.noreply.github.com>
2024-12-07 23:36:42 +0300

    last commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QPainter, QColor
+import random
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.flag = False
+        self.pushButton.clicked.connect(self.draw_flag)
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            x = random.randint(1, 200)
+            y = random.randint(1, 200)
+            qp.setBrush(QColor(255, 255, 0))
+            qp.drawEllipse(x, x, y, y)
+            qp.end()
+
+    def draw_flag(self, qp):
+        self.flag = True
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
